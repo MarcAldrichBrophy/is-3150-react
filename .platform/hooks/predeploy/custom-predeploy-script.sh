f624aad7b898b9367cd60df58eddb9e335cdf577
Marc Brophy <aldrichbrophy@gmail.com>
2023-05-07 19:30:35 -0700

    added bash if statement.

--- .platform/hooks/predeploy/custom-predeploy-script.sh
+++ .platform/hooks/predeploy/custom-predeploy-script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo su
 cd /var/app/current
-mkdir node_modules
-npm install --omit=dev
+
+if [ ! -e "./node_modules" ]; then
+  mkdir node_modules
+  npm install --omit=dev
+fi
+
 echo "PREDEPLOY COMPLETED"
